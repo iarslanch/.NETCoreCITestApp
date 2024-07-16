b6a0b6e48e2610b11b05f22ee4770aa33253a8b3
Muhammad Arslan <106810978+iarslanch@users.noreply.github.com>
2024-07-16 21:57:39 +0500

    Add new code

--- CITestApp/Class1.cs
+++ CITestApp/Class1.cs
@@ -1,4 +1,9 @@
-﻿using System.Xml.Linq;
+﻿using System.Data.Entity;
+using System.Diagnostics;
+using System.Net;
+using System.Security.Cryptography;
+using System.Xml.Linq;
+
 
 namespace CITestApp
 {
@@ -8,7 +13,47 @@ namespace CITestApp
         string password = "Admin123"; // Sensitive
         string usernamePassword = "user=admin&password=Admin123"; // Sensitive
         string url = "scheme://user:Admin123@domain.com"; // Sensitive
+        private static string TargetDirectory = "/path/to/target/directory/";
 
+        //public void Foo(DbContext context, string query, string param)
+        //{
+        //    string sensitiveQuery = string.Concat(query, param);
+        //    context.Database.ExecuteSqlCommand(sensitiveQuery); // Sensitive
+        //    context.Query<User>().FromSql(sensitiveQuery); // Sensitive
+
+        //    context.Database.ExecuteSqlCommand($"SELECT * FROM mytable WHERE mycol={value}", param); // Sensitive, the FormattableString is evaluated and converted to RawSqlString
+        //    string query = $"SELECT * FROM mytable WHERE mycol={param}";
+        //    context.Database.ExecuteSqlCommand(query); // Sensitive, the FormattableString has already been evaluated, it won't be converted to a parametrized query.
+        //}
+
+        public void Example(string filename)
+        {
+            var ip = "192.168.12.42";
+            var address = IPAddress.Parse(ip);
+
+            Process p = new Process();
+            p.StartInfo.FileName = "binary"; // Sensitive
+
+
+            var urlHttp = "http://example.com";                 // Noncompliant
+            var urlFtp = "ftp://anonymous@example.com";         // Noncompliant
+            var urlTelnet = "telnet://anonymous@example.com";   // Noncompliant
+            string path = Path.Combine(TargetDirectory, filename);
+            System.IO.File.Delete(path);
+        }
+        public void Run(string binary)
+        {
+            var hashProvider1 = new MD5CryptoServiceProvider(); // Sensitive
+            var hashProvider2 = (HashAlgorithm)CryptoConfig.CreateFromName("MD5"); // Sensitive
+            var hashProvider3 = new SHA1Managed(); // Sensitive
+            var hashProvider4 = HashAlgorithm.Create("SHA1"); // Sensitive
+
+            Process p = new Process();
+            p.StartInfo.FileName = binary;
+            p.Start();
+        }
+        
+    
         public void DoSomething()
         {
             if (this is Class1) // Noncompliant
@@ -72,4 +117,5 @@ namespace CITestApp
         }
 
     }
+   
 }
